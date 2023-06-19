66e25aafc00a348d585370e17ec080f95c464eff
kidaneb <105313373+kidaneb@users.noreply.github.com>
2023-06-19 11:45:18 +0300

    add animation fn and format code with prettier

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ todoInput = document.getElementById("inputfield");
 addButton = document.getElementById("add-button");
 
 addButton.addEventListener("click", createTodoItem);
-todoInput.addEventListener('keypress', (e)=>{
-  if(e.code == 'Enter'){
+todoInput.addEventListener("keypress", (e) => {
+  if (e.code == "Enter") {
     createTodoItem();
   }
 });
@@ -32,16 +32,16 @@ function completeTodo(id) {
     .getElementById(`todo-description-${id}`)
     .classList.toggle("completed");
 }
-function removeTodo (id){
-  todos.forEach((todo, index) =>{
-    if(todo.id == id){
-      todos.splice(index,1);
+function removeTodo(id) {
+  todos.forEach((todo, index) => {
+    if (todo.id == id) {
+      todos.splice(index, 1);
     }
-  })
-  localStorage.setItem('todos', JSON.stringify(todos));
+  });
+  localStorage.setItem("todos", JSON.stringify(todos));
   document.getElementById(`todo-item-${id}`).remove();
 
-  console.log('1');
+  console.log("1");
 }
 
 function createtodoItemElement(id, description, complete) {
@@ -71,9 +71,10 @@ function createtodoItemElement(id, description, complete) {
   editButton.className = "edit";
   editButton.id = `edit-${id}`;
   editButton.innerHTML = "Edit";
-  editButton.addEventListener('click', () => {
+  editButton.addEventListener("click", () => {
     activeEditId = id;
-    displayEditBox(description)})
+    displayEditBox(description);
+  });
 
   const removeButton = document.createElement("button");
   removeButton.className = "remove";
@@ -82,14 +83,13 @@ function createtodoItemElement(id, description, complete) {
   removeButton.addEventListener("click", () => {
     activeId = id;
     displayDialogueBox(
-        'Delete Button clicked',
-        'Are you sure you want to delete this todo?',
-        true,
-        true,
-        'DELETE_TODO'
-        )
+      "Delete Button clicked",
+      "Are you sure you want to delete this todo?",
+      true,
+      true,
+      "DELETE_TODO"
+    );
   });
-  
 
   todoAction.appendChild(completeButton);
   todoAction.appendChild(editButton);
@@ -108,7 +108,7 @@ function createtodoItemElement(id, description, complete) {
 function createTodoItem() {
   todoDescription = todoInput.value;
 
-  if(validateInput(todoDescription.replace(/\s/g, ""))){
+  if (validateInput(todoDescription.replace(/\s/g, ""))) {
     todoItem = {
       id: id,
       description: todoDescription,
@@ -116,130 +116,148 @@ function createTodoItem() {
     };
     id++;
     localStorage.setItem("id", JSON.stringify(id));
-  
+
     todos.push(todoItem);
     localStorage.setItem("todos", JSON.stringify(todos));
     createtodoItemElement(todoItem.id, todoItem.description, todoItem.complete);
-  
-    todoInput.value = "";
-  }
-  else{
-    
-  }
 
+    todoInput.value = "";
+  } else {
+  }
 }
-function validateInput(input){
-  if(input === ""){
-    displayDialogueBox('Input Error', 'Input can not be empty', false, true, 'INPUT_ERROR');
+function validateInput(input) {
+  if (input === "") {
+    displayDialogueBox(
+      "Input Error",
+      "Input can not be empty",
+      false,
+      true,
+      "INPUT_ERROR"
+    );
     return false;
-  }
-  else if(input.length < 5){
+  } else if (input.length < 5) {
     return false;
-  }
-  else{
+  } else {
     return true;
   }
 }
 
 // CREATE NEW FUNCTIONS
-let MODE ='';
-let ACTIVE_MODE='';
-const mainContainer = document.getElementById('main-container');
-const dialogueBox = document.getElementById('dialogue');
-const closeButton = document.getElementById('close');
-const dialogueTitle = document.getElementById('title');
-const dialogueBody = document.getElementById('body');
-const cancelButton =  document.getElementById('cancel');
-const acceptButton = document.getElementById('accept');   
-const background = document.getElementById('background');
+let MODE = "";
+let ACTIVE_MODE = "";
+const mainContainer = document.getElementById("main-container");
+const dialogueBox = document.getElementById("dialogue");
+const closeButton = document.getElementById("close");
+const dialogueTitle = document.getElementById("title");
+const dialogueBody = document.getElementById("body");
+const cancelButton = document.getElementById("cancel");
+const acceptButton = document.getElementById("accept");
+const background = document.getElementById("background");
 
-background.addEventListener('click', bg)
-function bg(){
-  if(ACTIVE_MODE === 'DELETE'){
-  closeDialogueBox();}
-  else if (ACTIVE_MODE === 'EDIT'){
-  closeEditBox();}
-  mainContainer.style.zIndex = '10';
-  background.style.zIndex = '5';
-  editBox.style.zIndex = '0';
-}
-
-
-acceptButton.addEventListener('click', () => {
-  if (MODE === 'INPUT_ERROR') {
-    closeDialogueBox();
-  }
-  if (MODE === 'DELETE_TODO') {
-    console.log('1');
-    removeTodo(activeId);
-    closeDialogueBox();
+background.addEventListener("click", () => {
+  {
+    if (ACTIVE_MODE === "DELETE") {
+      closeDialogueBox();
+    } else if (ACTIVE_MODE === "EDIT") {
+      closeEditBox();
+    } else {
+      return;
+    }
+    mainContainer.style.zIndex = "10";
+    background.style.zIndex = "5";
+    editBox.style.zIndex = "0";
   }
 });
 
-cancelButton.addEventListener('click', closeDialogueBox);
-closeButton.addEventListener('click', closeDialogueBox);
+acceptButton.addEventListener("click", () => {
+  if (MODE === "INPUT_ERROR") {
+    closeDialogueBox();
+  }
+  if (MODE === "DELETE_TODO") {
+    console.log("1");
 
+    closeDialogueBox();
+    deleteAnimation(activeId);
+  }
+});
 
-function displayDialogueBox(
-  title,
-  body,
-  cancel_visible,
-  accept_visible,
-  mode
-  ){
-    MODE = mode;
-    ACTIVE_MODE = 'DELETE';
+cancelButton.addEventListener("click", closeDialogueBox);
+closeButton.addEventListener("click", closeDialogueBox);
+
+function displayDialogueBox(title, body, cancel_visible, accept_visible, mode) {
+  MODE = mode;
+  ACTIVE_MODE = "DELETE";
   dialogueTitle.innerText = title;
   dialogueBody.innerText = body;
 
   cancel_visible
-      ?(cancelButton.style.display = 'flex')
-      :(cancelButton.style.display = 'none');
+    ? (cancelButton.style.display = "flex")
+    : (cancelButton.style.display = "none");
   accept_visible
-      ?(acceptButton.style.display = 'flex')
-      :(acceptButton.style.display = 'none');
+    ? (acceptButton.style.display = "flex")
+    : (acceptButton.style.display = "none");
 
-  dialogueBox.style.display = 'flex';
-  mainContainer.style.opacity = '0.2';
-  mainContainer.style.zIndex = '0';
-  background.style.zIndex = '5';
-  dialogueBox.style.zIndex = '10';
-
+  dialogueBox.style.display = "flex";
+  mainContainer.style.opacity = "0.2";
+  mainContainer.style.zIndex = "0";
+  background.style.zIndex = "5";
+  dialogueBox.style.zIndex = "10";
 }
-function closeDialogueBox(){
-  dialogueBox.style.display = 'none';
-  mainContainer.style.opacity = '1';
+function closeDialogueBox() {
+  dialogueBox.style.display = "none";
+  mainContainer.style.opacity = "1";
 }
 
 //EDIT FUNCTION
-const editBox = document.getElementById('edit-box');
-const editCloseButton = document.getElementById('edit-close-button');
-const editInput = document.getElementById('edit-input');
-const saveButton = document.getElementById('save-button');
+const editBox = document.getElementById("edit-box");
+const editCloseButton = document.getElementById("edit-close-button");
+const editInput = document.getElementById("edit-input");
+const saveButton = document.getElementById("save-button");
 
-saveButton.addEventListener('click', () => {
+saveButton.addEventListener("click", () => {
   saveEditedTodo(activeEditId);
-})
+});
 
-editCloseButton.addEventListener('click', closeEditBox);
+editCloseButton.addEventListener("click", closeEditBox);
 
-function saveEditedTodo(id){
-
+function saveEditedTodo(id) {
   document.getElementById(`todo-description-${id}`).innerText = editInput.value;
   closeEditBox();
 }
-function displayEditBox(description){
-  ACTIVE_MODE = 'EDIT';
-  editBox.style.display = 'flex';
+function displayEditBox(description) {
+  ACTIVE_MODE = "EDIT";
+  editBox.style.display = "flex";
   editInput.value = description;
   editInput.focus();
-  mainContainer.style.opacity = '0.2';
-  mainContainer.style.zIndex = '0';
-  background.style.zIndex = '5';
-  editBox.style.zIndex = '10';
+  mainContainer.style.opacity = "0.2";
+  mainContainer.style.zIndex = "0";
+  background.style.zIndex = "5";
+  editBox.style.zIndex = "10";
 }
 
-function closeEditBox(){
-  editBox.style.display = 'none';
-  mainContainer.style.opacity = '1';
+function closeEditBox() {
+  editBox.style.display = "none";
+  mainContainer.style.opacity = "1";
+}
+
+//ADDING ANIMATION TO IT
+let pos = 0;
+function deleteAnimation(activeId) {
+  pos += 1;
+
+  if (pos < 615) {
+    document.getElementById(
+      `todo-description-${activeId}`
+    ).style.transform = `translateX(${pos}px)`;
+    document.getElementById(
+      `todo-action-${activeId}`
+    ).style.transform = `translateX(${pos}px)`;
+
+    requestAnimationFrame(() => deleteAnimation(activeId));
+    console.log(pos);
+  } else if (pos === 615) {
+    removeTodo(activeId);
+    pos = 0;
+    return;
+  }
 }
